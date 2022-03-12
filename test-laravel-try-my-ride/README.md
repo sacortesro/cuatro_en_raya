0cad5c6a44fdcfa02d4ed29379711d83024689e5
sacortesro <sacortesro@unal.edu.co>
2022-03-12 01:32:01 -0500

    Aplicación con autenticación simple

---
+++ test-laravel-try-my-ride/README.md
@@ -0,0 +1,29 @@
+# API simple authentication
+
+Esta API se desarrolló en laravel y laravel sanctum, sqlite. Genera un token de autenticación cuando se registra e inicia sesión un nuevo usuario
+
+## Installación
+
+Para correr el proyecto es necesario tener instalado PHP, laravel, laravel sanctum, sqlite.
+
+Si no está instalado sqlite 
+```bash
+  sudo apt install php-squlite3
+  composer install
+  npm install
+  cp .env.example .env
+  php artisan key:generate
+```
+
+Crear una base de datos vacia de nombre dabase.sqlite en el foder database. Revisar que las variables de base de datos sean las correctas en el archivo .env.
+
+
+```bash
+    echo database/datasbase.sqlite
+    php artisan migrate
+    php artisan serve
+```
+
+
+    
+
---
+++ test-laravel-try-my-ride/app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|string ',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $validateData['name'],
+            'email' => $validateData['email'],
+            'password' => Hash::make($validateData['password'])
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login data'
+            ], status: 401);
+        }
+        
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+
+    public function infouser(Request $request)
+    {
+        return $request->user();
+    }
+}
